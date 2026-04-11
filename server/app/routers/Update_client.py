8ca648bdf33a297996e5537e670b7c86d3311465
Rodrigo <rodrioppo2006@gmail.com>
2026-04-11 08:55:21 +0200

    solve modelsClient importation error

--- server/app/routers/Update_client.py
+++ server/app/routers/Update_client.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+#Tienes que importar la clase no el archivo
 from ..schemas.request.Client import Client
 from ..database import models
 from ..database.database import get_db
@@ -11,6 +12,7 @@ router = APIRouter(
 
 @router.post('/')
 def create_client(client : Client, db: Session = Depends(get_db)):
-    new_client = models.Client(name=client.name, email=client.email, phone_number=client.phone_number)
+    #Tienes que usar la clase no el archivo por eso pongo dos veces Client(una por el archivo y otra por la clase)
+    new_client = models.Client.Client(name=client.name, email=client.email, phone_number=client.phone_number)
     db.add(new_client)
     db.commit() 
