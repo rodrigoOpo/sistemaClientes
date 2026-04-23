69984b3e0f8af9e23a84e6516fcea7b88d4c66cd
Rodrigo <rodrioppo2006@gmail.com>
2026-04-23 20:39:03 +0200

    create listener && parseFunction/connectionBridge

--- server/app/api.py
+++ server/app/api.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Websocket, WebsocketDisconnect
 from .routers import Update_client
 from .database.models import Client
 from .database.database import Base, engine
 from fastapi.middleware.cors import CORSMiddleware
+from typing import Set
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+listeners: Set[Websocket] = set()
+
+#For the table connection we need 2 technologies
+#We need them because the client will not ask for the data, so the database and later the api will send data to the client
+#1)LISTEN/NOTIFY in the database
+#2)Websockets in the api
 
 app.add_middleware(
     CORSMiddleware,
