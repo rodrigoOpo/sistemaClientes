bb0466ac008c6c8d02fe24558121ccf21a727504
Rodrigo <rodrioppo2006@gmail.com>
2026-04-29 21:12:23 +0200

    finall fix url

--- client/src/app/api.ts
+++ client/src/app/api.ts
@@ -9,9 +9,7 @@ export const createClient = async (client: Omit<Client, "_id">) => {
         body: JSON.stringify(client)
     }
 
-    console.log("URL REAL:", 'https://sistemaclientes-7ulo.onrender.com/clients/');
-    console.log("ENV:", process.env.NEXT_PUBLIC_API_URL);
-    
+
     const res = await fetch('https://sistemaclientes-7ulo.onrender.com/clients/', options)
     const data = await res.json()
     return clientSchema.parse(data)
