379a49f035ed0cc054bc942cfd171dec6509716f
Rodrigo <rodrioppo2006@gmail.com>
2026-05-09 15:54:11 +0200

    change importation page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,11 @@
 import Form from "@/ui/Form";
-import {Table} from "@/components/ui/table";
+import { ClientsTable } from "@/components/ui/table";
 
 export default function Home() {
   return (
     <div className=" text-3xl bg-black md:h-screen">
       <Form/>
-      <Table />
+      <ClientsTable />
     </div>
   );
 }
